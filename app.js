8a6805381cbb6a164b3e6fc29207bde11e301f3b
Love Babbar <lovebabbar@Loves-MacBook-Air.local>
2024-01-11 04:42:22 +0530

    MongoDB connection added

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
+
+// Replace <YOUR_CONNECTION_STRING> with your MongoDB Atlas connection string
+const connectionString = 'mongodb+srv://lovebabbar3:rr8opKHOeCmHdChN@cluster0.c0k4xvu.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB Atlas
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error.message);
+  });
+
+
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello, this is my Node.js app!');
